babf7eb85ddae3fe3fed36aeb8a8b1ece17e16c6
syteser <savchenko.sergey.v@gmail.com>
2023-11-05 10:17:33 +0200

    8 коммит 1 - добавлен раздел Хелп 2 - Оптимизация кода

--- src/Main.java
+++ src/Main.java
@@ -29,8 +29,7 @@ public class Main {
                     c += sdvig[i];
                     fileWriter.write(c);
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
@@ -44,8 +43,7 @@ public class Main {
                     c -= sdvig[i];
                     fileWriter.write(c);
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
@@ -58,27 +56,38 @@ public class Main {
             return false;
         }
 
-        if (!args[0].equalsIgnoreCase("code") && !args[0].equalsIgnoreCase("decode")) {
-            System.out.println("Возможные параметры code или decode");
+        if (args.length == 1 && args[0].equalsIgnoreCase("help") || (args.length == 1 && args[0].equals("?"))) {
+            System.out.println("Welcome to help!");
+            System.out.println("Возможные параметры - code или decode");
+            System.out.println("Программа принимает с коммандной строки 4 параметра:");
+            System.out.println("1 - тип - кодировать или дектодировать (code decode)");
+            System.out.println("2 - имя исходного файла (тхт)");
+            System.out.println("3 - имя финального (закодированого) файла");
+            System.out.println("4 - ключевая фраза 1......int длины");
+            System.out.println("пример: code d:\\in.txt d:\\out.txt Ключевая_фраза_любой_длины_из_любых-символов....НО!!!!Без+пробелов,слешей_и_кавычек..");
             return false;
         }
 
-        if ((args.length == 1 && args[0].equalsIgnoreCase("help")) || (args.length == 1 && args[0].equals("?"))) {
-            System.out.println("Welcome to help!");
-            System.out.println("Возможные параметры code или decode");
+        if (args.length==4 && !args[0].equalsIgnoreCase("code") && !args[0].equalsIgnoreCase("decode")) {
+            System.out.println("Возможные параметры - code или decode");
+            return false;
         }
 
-        if (args[1].equalsIgnoreCase(args[2])) {
+
+        if (args.length == 4 && args[1].equalsIgnoreCase(args[2])) {
             System.out.println("Исходный файл не может совпадать с финальным файлом!");
             return false;
         }
 
-        mode = args[0];
-        fileIn = args[1];
-        fileOut = args[2];
-        key = args[3];
+        if(args.length==4){
+            mode = args[0];
+            fileIn = args[1];
+            fileOut = args[2];
+            key = args[3];
+            return true;
+        }
 
-        return true;
+        return false;
     }
 
 }
